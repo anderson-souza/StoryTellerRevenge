deff89fe495beb9bfbbd407fc2f887119ee849f9
Anderson <andersonpds14@gmail.com>
2017-10-09 10:59:00 -0300

    Initial Repo

---
+++ Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour {
+
+    GameObject _backgroundImage;
+    // Use this for initialization
+    void Start() {
+        if (!_backgroundImage) {
+            _backgroundImage = gameObject.transform.Find("selectionBackground").gameObject;
+            _backgroundImage.GetComponent<Image>().fillAmount = 0;
+            Debug.Log("Encontrei a imagem: " + _backgroundImage);
+        }
+    }
+
+    public void OnGazeEnter() {
+        StartCoroutine("BackgroundTransition");
+    }
+
+    public void OnGazeExit() {
+        StopCoroutine("BackgroundTransition");
+        _backgroundImage.GetComponent<Image>().fillAmount = 0;
+        Debug.Log("Entrei no onGazeExit");
+    }
+
+    private IEnumerator BackgroundTransition() {
+        float start = Time.time;
+        float elapsed = 0;
+        float duration = 1.5f;
+        while (elapsed < duration) {
+            elapsed = Time.time - start;
+            float normalisedTime = Mathf.Clamp(elapsed / duration, 0, 1);
+            _backgroundImage.GetComponent<Image>().fillAmount = Mathf.Lerp(0, 1, normalisedTime);
+            yield return null;
+        }
+        GetComponent<Button>().onClick.Invoke();
+    }
+}
---
+++ Assets/VideoSphereController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoSphereController : MonoBehaviour
+{
+    private VideoPlayer _videoPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_videoPlayer == null)
+        {
+            _videoPlayer = GameObject.FindObjectOfType<VideoPlayer>();
+        }
+    }
+
+    public void PlayVideo()
+    {
+        _videoPlayer.Play();
+    }
+
+    public void PauseVideo()
+    {
+        _videoPlayer.Pause();
+    }
+
+    public void RestartVideo()
+    {
+        _videoPlayer.Stop();
+        _videoPlayer.Play();
+    }
+    
+}
